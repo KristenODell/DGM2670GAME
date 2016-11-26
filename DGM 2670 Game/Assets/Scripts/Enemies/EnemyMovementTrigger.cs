2eaf283680cb2d1d43c0f2d3af7fe73af8de2fd2
Unknown <little_miss_kris07@hotmail.com>
2016-11-26 14:30:58 -0700

    PlayerMovement

---
+++ DGM 2670 Game/Assets/Scripts/Enemies/EnemyMovementTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovementTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnTriggerEnter()
+     {
+
+     }
+}
---
+++ DGM 2670 Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController myCC;
+    public float speed = 8;
+    Vector3 tempPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myCC = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        tempPos.x = speed * Input.GetAxis("Horizontal");
+        tempPos.z = speed * Input.GetAxis("Vertical");
+        myCC.Move(tempPos * Time.deltaTime);
+    }
+}
